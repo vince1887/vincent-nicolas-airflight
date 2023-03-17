51856cc874e4006ebd282fb4a5307b29072dda1c
Vanounais <89648070+Vanounais@users.noreply.github.com>
2023-03-17 05:53:45 -0400

    fwefw

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 var swiper = new Swiper(".mySwiper", {});
+
+var swiperlp = new Swiper(".swiper-screenshots", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 25
+        }}
+  });
